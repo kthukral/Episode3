daee4015646fc5e59cd1a0f8b133306eb43e9c20
Karan Thukral <karanthukral@gmail.com>
2013-12-11 22:04:31 -0500

    finished

--- db/seed.rb
+++ db/seed.rb
@@ -1,7 +1,15 @@
 # Cleaning Out
 Network.delete_all
 Show.delete_all
+Movie.delete_all
+
 amc = Network.create(name: "AMC")
 nbc = Network.create(name: "NBC")
+cw = Network.create(name: "CW")
 Show.create(name: "Mad Men", day_of_week: "Sunday", hour_of_day: 22, network: amc)
 Show.create(name: "Community", day_of_week: "Thursday", hour_of_day: 20, network: nbc)
+Show.create(name: "Arrow", day_of_week: "Wednesday", hour_of_day: 21, network: cw)
+Show.create(name: "The Big Bang Theory", day_of_week: "Thrusday", hour_of_day: 20, network: nbc)
+
+Movie.create(name: "Hunger Games", leadActor: "Josh Hutcherson", leadActress: "Jennifer Lawrance", year_of_release: 2013)
+Movie.create(name: "My Movie", leadActor: "DEF", leadActress: "ABC", year_of_release: 2013)
--- watchman.rb
+++ watchman.rb
@@ -1,15 +1,50 @@
 require 'rubygems'
 require 'bundler/setup'
-
+include Gem::UserInteraction
 require "./db/setup"
 Dir.glob('./models/*').each { |r| require r}
 require "./db/seed"
 
-puts "There are #{Show.count} in the database"
+puts "There are #{Movie.count} in the database"
 
-Network.all.each do |network|
-	puts "Shows airing on #{network}"
-	network.shows.each do |show|
-		puts show
-	end	
+#user_day_of_week = ui.ask("What day would you like to watch TV?")
+at_least_one = false;
+
+#Show.all.each do |show|
+#	same_or_not = user_day_of_week.capitalize <=> show.day_of_week.capitalize
+#	if same_or_not == 0
+#		at_least_one = true;
+#		puts show
+#	end
+#end
+
+#if !at_least_one
+#	puts "Sorry no show was found for that day"
+#end
+
+#Network.all.each do |network|
+#	puts "Shows airing on #{network}"
+#	network.shows.each do |show|
+#		puts show
+#	end	
+#end
+
+Movie.all.each do |movie|
+	puts "#{movie.name}"
+end
+
+user_movie_name = ui.ask ("Which Movie do you want to expand?")
+at_least_one = false;
+
+Movie.all.each do |movie|
+	same_or_not = user_movie_name.capitalize <=> movie.name.capitalize
+	if same_or_not == 0
+		puts "The movie #{movie.name} was released in #{movie.year_of_release}
+		and had #{movie.leadActor} and #{movie.leadActress}."
+		at_least_one = true
+	end
+end
+
+if !at_least_one
+	puts "wrong name..."
 end
